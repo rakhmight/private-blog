0ab8dd158a7810fac153c0da21c71ecb0a78f306
rakh <mr.rakhimov@mail.com>
2024-05-08 10:50:55 +0500

    fix bugs & add README.md

--- src/main.js
+++ src/main.js
@@ -8,14 +8,15 @@ import firebase from "firebase/compat/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDkr3YgPHMKxUIcI6cjZYRlET6PNF_Ky9Y",
-  authDomain: "private-blog-12658.firebaseapp.com",
-  projectId: "private-blog-12658",
-  storageBucket: "private-blog-12658.appspot.com",
-  messagingSenderId: "1075378531610",
-  appId: "1:1075378531610:web:81b368e7cb785cb0e610a3",
-  databaseURL: "https://private-blog-12658.firebaseio.com",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  databaseURL: "",
 };
+
 const app = firebase.initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
